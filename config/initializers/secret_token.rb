b9499f4aa1d7b5213cd0bfbc8e4405fade30e3f5
Christopher Guthrie <christopher.a.guthrie@gmail.com>
2014-03-14 13:53:38 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChrisBlogRails::Application.config.secret_key_base = '196b03963fe1db52ae40e069df73a49ff7657c8229c225fd81b2a08628fbae1c59170103cd5eacae6710917c1bf9524f0748fbe833a3df8d0ef9f8f3b7ed9cd1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ChrisBlogRails::Application.config.session_store :cookie_store, key: '_chris_blog_rails_session'
